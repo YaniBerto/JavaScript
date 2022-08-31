dde4bd860b4f9ccd4871ebe06284e263260cbb20
YaniBerto <yanina.bertolotti@gmail.com>
2022-08-31 09:18:05 -0300

    realice cambios para hacer una operacion

--- shop.js
+++ shop.js
@@ -1,77 +1,20 @@
-//console.log("hola")
-//let precio = 100
-//let cantidad = 2
-//console.log(precio * cantidad)
-//prompt ("ingrese monto")
-//prompt("ingrese cantidad")
-//alert("ingrese monto + ingrese cantidad")
-//console.log(`precio` + `cantidad`)
-
-//let cantGoles= parseInt(prompt("ingrese cantidad de goles anotados"))
-//let cantPartidos= parseInt(prompt("ingrese cantidad de partidos jugados"))
-//let promedio= cantGoles/cantPartidos
-//alert(`el promedio es ${promedio}`)
-
-//let preguntaEdad =prompt("usted es mayor de edad?")
-//if (preguntaEdad =="si") {
-//    console.log("usted es mayor de edad puede comprar")
-//}
-//else{console.log("usted no puede comprar")}
-//let color = "amarillo"
-//if (color == "rojo"){console.log("el color es rojo")}
-//else if (color=="azul"){console.log("el color es primario")}
-//else if (color=="amarillo"){console.log("el color es primario")}
-
-//if ((color == "rojo") || (color == "azul") || (color == "amarillo"))
-//{console.log(`el color es ${color} y es primario`)}
-//else {
-//    console.log(`este color es ${color} y no es primario`)}
-//let boolean = false
-//let condicion=true
-//let contador =0
-
-
-//let precio= 2;
-//let mayorAdiez = (precio > 10)
-//console.log(mayorAdiez)
-
-//let numMultiplicar = parseInt(prompt("ingrese en num"))
-//for(let index=0; index <= 10; index ++ )
-//{console.log(numMultiplicar*index)}
-//console.log(`${numMultiplicar}x{$index)=${multiplicacion}´)
-//let entrada=prompt ("Ingresar")
-//while (entrada !="ESC")
-//console.log("el usuario ingreso" + entrada)
-//let autor = "borges"
-//let precioLibro = 3000
-//let titulo ="ficciones"
-
-//if ((autor == "borges") && (precioLibro == 2000) && (titulo == "ficciones")){console.log("es su libro deseado")}
-//else {console.log("no es su libro favorito")}
-
-//let respuesta = prompt("quiere ingresar un libro?RTA:si o no")
-
-//while(respuesta.toLowerCase() == "si"){
-//let autor = prompt("ingrese autor")
-//let titulo = prompt("ingrese titulo")
-
-//console.log(`el autor es ${autor}y el ttitalo es ${titulo}`)
-
-//respuesta = prompt ("quiere otro libro?rta si o no")}
-
 let preguntaCliente =prompt("Busca un producto para un adulto?")
 if (preguntaCliente =="si") {
 console.log("éstos son nuestros productos para adultos")
+
+let respuesta=prompt ("monopatin?Respuesta: si o no")
+do{ let precio=prompt("ingrese precio")
+    const iva=(1.21)
+    respuesta=alert ("el precio con IVA es" +" "+ "$" + precio*iva)
 }
+
+while(respuesta=="si")} 
 else{console.log("estos son nuestros productos para niños")}
 
 
-let respuesta=prompt ("monopatin?Respuesta: si o no")
-do{ let marca=prompt("ingrese marca")
-    let rodado=prompt("ingrese rodado")
-    respuesta=prompt ("quiere otro producto?Respuesta si o no") 
-}
-while(respuesta=="si"){
-    } 
 
-   
+  
+
+
+
+
