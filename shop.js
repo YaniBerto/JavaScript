9f0254dd7fadacd4259995bbc9563ff199d3753b
YaniBerto <yanina.bertolotti@gmail.com>
2022-09-02 21:02:29 -0300

    lestilos grales al index y utilice el tolowercase

--- shop.js
+++ shop.js
@@ -1,5 +1,5 @@
 let preguntaCliente =prompt("Busca un producto para un adulto?")
-if (preguntaCliente =="si") {
+if (preguntaCliente.toLowerCase () == "si") {
 console.log("éstos son nuestros productos para adultos")
 
 let respuesta=prompt ("monopatin?Respuesta: si o no")
@@ -8,12 +8,10 @@ do{ let precio=prompt("ingrese precio")
     respuesta=alert ("el precio con IVA es" +" "+ "$" + precio*iva)
 }
 
-while(respuesta=="si")} 
+while(respuesta.toUpperCase() == "si")} 
 else{console.log("estos son nuestros productos para niños")}
 
 
-
-  
 
 
 
