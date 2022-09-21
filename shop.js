d6f18ef369a65de2e59c6a0ec8d36d1cc12a7689
YaniBerto <yanina.bertolotti@gmail.com>
2022-09-20 22:07:00 -0300

    practicas

--- shop.js
+++ shop.js
@@ -7,17 +7,17 @@
     //while (respuesta == "si")
 //}
 class Articulo {
-constructor(Marca, precio, boton) {
+constructor(Marca, precio, btn) {
         this.Marca = Marca,
-        this.precio = precio
-        this.boton = boton
-}}
+        this.precio = precio,
+        this.btn = btn
+    }}
+
 let boton = document.getElementsByClassName("boton")
 for(let compra of boton){
     compra.addEventListener("click", ()=>{
     alert(`Agregado al carrito`)
-    })
-  
+    })}
 
 const Articulo1 = new Articulo("Fiat", 50.000, "b1")
 const Articulo2 = new Articulo("kany", 75.000, "b2")
@@ -32,28 +32,21 @@ const Arts = []
 Arts.push(Articulo1, Articulo2, Articulo3, Articulo4, Articulo5, Articulo6, Articulo7, Articulo8)
 
 let botonBusqueda = document.getElementById("btnBuscar")
-botonBusqueda.onclick = buscarPorMarca()
-
+botonBusqueda.addEventListener('click',buscarPorMarca)
 function buscarPorMarca() {
-let busqueda = document.querySelector(".busqueda")
-Arts.filter((brand) => brand.Marca.toLowerCase() == buscarMarca.toLowerCase())
-    if (busqueda.length == 0) { alert("No se encontró ningún artículo") }
-    else { console.log(`los articulos encontrados son:`) }}}
+    let busqueda = document.getElementById("busqueda") 
+    console.log(busqueda.value)
 
-    buscarPorMarca()
-//for (let articuloencontrado of busqueda) {
-//        console.log(articuloencontrado.mostrarArts())
-//    }
-//}
-//for (let props of Arts) {
-//    props.mostrarArts()
-//}
+    let marcaingresada = Arts.filter((brand) => brand.Marca.toLowerCase() == busqueda.value.toLowerCase())
 
-//function verModelos() {
-//    alert("En la consola puede ver los modelos")
-//    console.log("disfrute nuestro catalogo:")
-// //   Arts.forEach((modelo) => {
-//        modelo.mostrarArts()
-//    })
-//}
+    if(marcaingresada.length == 0) { alert("No se encontró ningún artículo") }
+    else {alert(`los articulos encontrados son:`)
+    marcaingresada.forEach((Articulo) => {
+    alert(Articulo.Marca)
+    })
+}
+}
+
+
+
 
