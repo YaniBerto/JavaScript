4508699b9b5fda7078331de4e0f32949db9ffe83
YaniBerto <yanina.bertolotti@gmail.com>
2022-10-23 12:33:15 -0300

    "intento de agregar funcion de autocomplete"

--- shop.js
+++ shop.js
@@ -9,6 +9,8 @@ class Articulo {
 }
 
 let Arts = []
+let compra = []
+let marcaingresada = []
 
 const cargarArticulos = async()=>{
     const response = await fetch("./articulos.json")
@@ -21,6 +23,7 @@ const cargarArticulos = async()=>{
 }
 
 localStorage.setItem("Arts", JSON.stringify(Arts)) 
+mostrarProductos(Arts)
 }
 
 cargarArticulos()
@@ -32,41 +35,43 @@ if(localStorage.getItem("Arts")){
 }else{
     console.log("seteando por primera vez")
     cargarArticulos()
-    window.location.reload()
+    
 }
-
-let divProductos = document.getElementById("Productos")
 
 function mostrarProductos(array){
-    array.forEach((Articulo)=>{
+    let divProductos = document.getElementById("Productos")
     divProductos.innerHTML = ""
+    array.forEach((Articulo)=>{
+        let nuevoProducto = document.createElement("div")
+        nuevoProducto.innerHTML =`<div id="${Articulo.Id}"class="card" style="width: 18rem;">
+            <img class="card-img-top" style="height:250px;" src="images/${Articulo.Imagen}" alt="${Articulo.Modelo}">
+            <div class="card-body">
+            <h4 class="card-title">${Articulo.Marca}</h4>
+            <p class="card-text">$${Articulo.precio}</p>
+            <button id="agregarBtn${Articulo.Modelo}">Añadir al carrito</button>
+            </div>
+            </div>`
 
-    let nuevoProducto = document.createElement("div")
-nuevoProducto.innerHTML =`<div id="${Articulo.Id}"class="card" style="width: 18rem;">
-                        <img class="card-img-top" style="height:250px;" src="images/${Articulo.Imagen}" alt="${Articulo.Modelo}">
-                        <div class="card-body">
-                        <h4 class="card-title">${Articulo.Marca}</h4>
-                        <p class="card-text">${Articulo.precio}</p>
-                        <button id="agregarBtn${Articulo.Modelo}">Añadir al carrito</button>
-                        </div>
-    </div>`
-    divProductos.append(nuevoProducto)
+        divProductos.append(nuevoProducto)
 
-    let agregarBtn = document.getElementById(`agregarBtn${Articulo.Modelo}`)
-        agregarBtn.addEventListener("click", ()=>{
-        agregarAlCarrito(Articulo)
+        let agregarBtn = document.getElementById(`agregarBtn${Articulo.Modelo}`)
+            agregarBtn.addEventListener("click", ()=>{
+            agregarAlCarrito(Articulo)
+        })
     })
-})
 }
 
-
-mostrarProductos(productos2)
-
-    function agregarAlCarrito(Articulo){
+function agregarAlCarrito(Articulo){
         let articuloAgregado = productosEnCarrito.find((elem)=> (elem.Id == Articulo.Id))
-                if (articuloAgregado == undefined){
-                productosEnCarrito.push(Articulo)
-            localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+                if (articuloAgregado == null){
+                    let item = {
+                        articulo: Articulo,
+                        cantidad: 1
+                    }
+                    compra.push(item)
+                    productosEnCarrito.push(Articulo)
+                localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+                localStorage.setItem("compra", JSON.stringify(compra))
             Toastify({
                 text: "El producto ha sido agregado al carrito",
                 duration:2000,
@@ -77,6 +82,12 @@ mostrarProductos(productos2)
                 }
             }).showToast();
         }else{
+            compra.forEach(element =>{
+                if(element.articulo.Id == Articulo.Id){
+            element.cantidad ++
+        }
+    })
+    localStorage.setItem("compra", JSON.stringify(compra))
         Toastify({
             text:"El producto ya se encuentra en el carrito",
             duration: 2000,
@@ -88,32 +99,63 @@ mostrarProductos(productos2)
         }
         }).showToast();
 }
-
+console.log(compra)
+}
 let botonBusqueda = document.getElementById("btnBuscar")
+
 botonBusqueda.addEventListener('click',buscarPorMarca)
+let input = document.getElementById("busqueda")
+input.addEventListener("keypress", function(event){
+    if (event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("btnBuscar").click();
+        }
+        
+});
 
 function buscarPorMarca() {
-    let busqueda = document.getElementById("busqueda") 
-    console.log(busqueda.value)
-    let marcaingresada = Arts.filter((brand) => brand.Marca.toLowerCase() == busqueda.value.toLowerCase())
 
-    marcaingresada.length == 0 ?  
+    let marcaingresada = Arts.filter((brand) => brand.Marca.toLowerCase() == busqueda.value.toLowerCase())
+    localStorage.setItem("marcaingresada",JSON.stringify(marcaingresada))
+    input.value= ""
+    marcaingresada.length == 0 ?
         Swal.fire({
         icon: 'error',
         title: 'Oops...',
-        text: 'No se encontró ningún artículo de esa marca',
-        }) 
-        : 
-    marcaingresada.forEach((Articulo) => {
+        text: 'No se encontró ningún artículo de esa marca'
+        })
+    :marcaingresada.forEach((Articulo) =>{
         Swal.fire({
         imageUrl: src=`images/${Articulo.Imagen}`,
-        text: `El modelo encontrado es: ${Articulo.Modelo}`,
+        text: `El articulo encontrado es: ${Articulo.Modelo}`,
         imageHeight: 500,
-        imageAlt: 'A tall image'
+        imageAlt: 'A tall image',
         })
-    })}
-let input = document.getElementById("busqueda")
-    input.value = ""
+})
+
+console.log(marcaingresada)
+}
+function autoComplete(Article){
+    const Arts = [];
+    return Arts.filter((valor)=>{
+        const valorMinuscula = valor.toLowerCase()
+        const ArticleMinuscula= valor.toLowerCase()
+        return valorMinuscula.includes(ArticleMinuscula)
+    })
+}
+const campo = document.querySelector(`.campo`)
+const sugerencias = document.querySelector(`.sugerencias`)
+campo.addEventListener(`input`, ({target})=>{
+    const datosDelCampo = target.value
+    if (datosDelCampo.length){
+        const autoCompleteValores = autoComplete(datosDelCampo)
+        sugerencias.innertHTML=`${autoCompleteValores.map((value)=>{
+            return(
+                `<li>${value}</li>`)
+        }).join('')}
+    `}
+})
+
 
 let botonCarrito = document.getElementById("botonCarrito")
 let modalBody = document.getElementById("modal-body")
@@ -121,46 +163,66 @@ let botonFinalizarCompra = document.getElementById("botonFinalizarCompra")
 let parrafoCompra = document.getElementById('precioTotal')
 
 botonCarrito.addEventListener("click", ()=>{
-cargarProductosCarrito(productosEnCarrito)
+cargarProductosCarrito(compra)
 })
+
 function cargarProductosCarrito(array){
     
     modalBody.innerHTML =""
-    array.forEach((productoCarrito)=>{
+    array.forEach((element)=>{
+        if(element.cantidad>0){
+            let subtotal = element.cantidad * element.articulo.precio
         modalBody.innerHTML +=
-        `<div class="card border-primary mb-3" id ="productoCarrito${productoCarrito.Id}"
+        `<div class="card border-primary mb-3" id ="productoCarrito${element.articulo.Id}"
         style="max-width: 540px;">
-            <img class="card-img-top" src="images/${productoCarrito.Imagen}" alt="${productoCarrito.Modelo}">
+            <img class="card-img-top" src="images/${element.articulo.Imagen}" alt="${element.articulo.Modelo}">
             <div class="card-body">
-                <h4 class="card-title">${productoCarrito.Modelo}</h4>
-                <p class="card-text">$${productoCarrito.precio}</p><button class= "btn btn-danger" id="botonEliminar${productoCarrito.Id}"><i class="fas fa-trash-alt"></i></
-                button>
+                <h4 class="card-title">${element.articulo.Modelo}</h4>
+                <div class="card-text mb-1>${element.cantidad} x $${element.articulo.precio}</div>
+                <span>Subtotal: $${subtotal}</span>
+                <br>
+                <br>
+                <button class="btn btn-danger" id="botonEliminar${element.articulo.Id}"><i class="fas fa-trash-alt"></i></button>
             </div>
         </div>`
-    document.getElementById(`botonEliminar${productoCarrito.Id}`)
+        }else{
+            let card = document.getElementById(`productoCarrito${element.articulo.Id}`)
+            if(card){
+                card.remove()
+            }
+        }    
+    })
+    
+array.forEach(element => {
+    let btnEliminar = document.getElementById(`botonEliminar${element.articulo.Id}`)
+    if(btnEliminar){
+        btnEliminar.addEventListener("click", () => {
+            let id = element.articulo.Id
+            compra.forEach(element2 => {
+                if(element2.articulo.Id == id){
+                    element2.cantidad --
+                    localStorage.setItem("compra", JSON.stringify(array))
+                    cargarProductosCarrito(array)
+                }
+            })
+        })
+    }
 })
 
-array.forEach((productoCarrito, indice)=>{
-    document.getElementById(`botonEliminar${productoCarrito.Id}`).addEventListener("click",()=>{
-        array.splice(indice, 1)
-        localStorage.setItem("carrito", JSON.stringify(array))
-        let cardArticulo = document.getElementById(`productoCarrito${productoCarrito.Id}`)
-        cardArticulo.remove()
-        compraTotal(array)
-    })
-})
     compraTotal(array)
 }
+
 function compraTotal(array){
     let acumulador = 0
-    acumulador = array.reduce((acumulador, productoCarrito)=>{
-    return acumulador + productoCarrito.precio
+    acumulador = array.reduce((acumulador, element)=>{
+    return acumulador + element.cantidad*element.articulo.precio
     },0) 
     acumulador == 0?parrafoCompra.innerHTML = `<p>No hay productos en el carrito</p>`
     :
-    parrafoCompra.innerHTML = `El total de su carrito es ${acumulador}`
+    parrafoCompra.innerHTML = `El total de su carrito es $${acumulador}`
 }
 botonFinalizarCompra.addEventListener("click", ()=>{finalizarCompra()})
+
 function finalizarCompra(){
 Swal.fire({
         title:`Está seguro de realizar la compra?`,
@@ -191,4 +253,4 @@ Swal.fire({
     }
 })
 }
-}
+
